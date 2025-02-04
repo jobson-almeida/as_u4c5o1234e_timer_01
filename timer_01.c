3275489b4ffadaa4d113d7d0ef5277b6e9972e8f
Jobson Almeida <jobson.ra@outlook.com>
2025-02-03 22:33:41 -0300

    chore: inicializar e configurar as GPIOs destinadas aos LEDs como saídas

--- timer_01.c
+++ timer_01.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-
+// disposição dos LEDs na placa BitDogLab que disponho
+#define LED_RED 13
+#define LED_BLUE 12 // cor substituta ao amarelo ausente na placa
+#define LED_GREEN 11
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+
+    gpio_init(LED_BLUE);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
