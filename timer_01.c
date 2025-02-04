1df7c2644da2ca2280941c169271dbbcbf39b8a0
Jobson Almeida <jobson.ra@outlook.com>
2025-02-03 22:48:03 -0300

    feat: habilitar timer repetitivo e criar função callback

--- timer_01.c
+++ timer_01.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 
-// disposição dos LEDs na placa BitDogLab que disponho
 #define LED_RED 13
-#define LED_BLUE 12 // cor substituta ao amarelo ausente na placa
+#define LED_BLUE 12 // cor substituta ao amarelo ausente na placa de desenvolvimento
 #define LED_GREEN 11
+
+bool repeating_timer_callback(struct repeating_timer *t)
+{
+    printf("repeating_timer_callback\n");
+}
 
 int main()
 {
@@ -18,6 +23,13 @@ int main()
 
     gpio_init(LED_GREEN);
     gpio_set_dir(LED_GREEN, GPIO_OUT);
+
+    // armazenara as informações sobre o temporizador
+    struct repeating_timer timer;
+
+    // configura o time com delay de 3000 milisegundos 
+    // para cada uma das chamadas da função de callback
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer);
 
     while (true)
     {
